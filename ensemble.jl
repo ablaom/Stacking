938305d9abee0d30ae85ed333a4911baa31c7042
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-12-05 10:42:54 +1300

    update ensemble example

--- ensemble.jl
+++ ensemble.jl
@@ -1,6 +1,12 @@
-# Simple example of a homogeneous ensemble using learning networks
+# # Simple example of a homogeneous ensemble using learning networks
+
+# In this simple example, no bagging is used, so every atomic model
+# gets the same learned parameters, unless the atomic model training
+# algorithm has randomness, eg, DecisionTree with random subsampling
+# of features at nodes.
 
 using MLJ
+using Plots; pyplot()
 
 # learning network (composite model spec):
 
@@ -8,7 +14,7 @@ Xs = source()
 ys = source(kind=:target)
 
 atom = @load DecisionTreeRegressor
-atom.n_subfeatures = 4
+atom.n_subfeatures = 4 # to ensure diversity among trained atomic models
 
 machines = (machine(atom, Xs, ys) for i in 1:100)
 
@@ -21,10 +27,45 @@ yhat = sum([predict(m, Xs) for  m in machines]);
 
 # new composite model type and instance:
 
-one_hundred_trees = @from_network OneHundredTrees(atom=atom) <= yhat
+one_hundred_models = @from_network OneHundredModels(atom=atom) <= yhat
+
+# application to data
 
 X, y = @load_boston;
-evaluate(one_hundred_trees, X, y, measure=rms)
+
+# tune regularization parameter for a *single* tree:
+
+r = range(atom,
+          :min_samples_split,
+          lower=2,
+          upper=100, scale=:log)
+
+mach = machine(atom, X, y)
+
+curve = learning_curve!(mach,
+                        range=r,
+                        measure=mav,
+                        resampling=CV(nfolds=9))
+
+plot(curve.parameter_values, curve.measurements, xlab=curve.parameter_name)
+savefig("atom.png")
+
+# tune regularization parameter for all trees in ensemble simultaneously:
+
+r = range(one_hundred_models,
+          :(atom.min_samples_split),
+          lower=2,
+          upper=100, scale=:log)
+
+mach = machine(one_hundred_models, X, y)
+
+curve = learning_curve!(mach,
+                        range=r,
+                        measure=mav,
+                        resampling=CV(nfolds=9))
+
+plot(curve.parameter_values, curve.measurements, xlab=curve.parameter_name)
+savefig("ensemble.png")
 
 using Literate #src
 Literate.notebook(@__FILE__, @__DIR__) #src
